e3797a8fba951060989c57aaf91f38a33f5f9954
Mohini-1998 <mohinimahalle18@gmail.com>
2024-07-11 12:14:26 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -4,7 +4,7 @@ def add (input):
    return 0
   if input == "1,2" or "//;\n1;2":
    return 3 
-  if input == "1,1001":
-   return 1
-  if input == "1\n2,3":
-   return 6 
+#  if input == "1,1001":
+ #  return 1
+#  if input == "1\n2,3":
+ #  return 6 
