419ca72425f039582e2041325c6323096e1bdf17
Mohini-1998 <mohinimahalle18@gmail.com>
2024-07-23 11:04:43 +0530

    Update StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -14,8 +14,8 @@ class TestStringCalculator(unittest.TestCase):
         #def test_expectZeroForEmptyInput(self):
           #      self.assertEqual(add("1,1001"), 1)
                 
-       # def test_expectZeroForEmptyInput(self):
-          #      self.assertEqual(add("//;\n1;2"), 3)
+        def test_expectZeroForEmptyInput(self):
+               self.assertEqual(add("//;\n1;2"), 3)
                 
         #def test_expectZeroForEmptyInput(self):
             #    self.assertEqual(add("1\n2,3"),6);
