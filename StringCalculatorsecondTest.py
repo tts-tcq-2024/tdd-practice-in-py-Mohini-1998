1ed86999b6af76dfdade0457dfda52b67c6729ce
Mohini-1998 <mohinimahalle18@gmail.com>
2024-08-01 12:19:23 +0530

    StringCalculatorsecondTest.py

--- StringCalculatorsecondTest.py
+++ StringCalculatorsecondTest.py
@@ -1,1 +1,22 @@
+import unittest
+from StringCalculator import add
+class TestStringCalculator(unittest.TestCase):
+                
+        def test_expectZeroForEmptyInput(self):
+                self.assertEqual(add("1,2"), 3)
+                
+        #def test_expectZeroForEmptyInput(self):
+               # self.assertEqual(add("1,1001"), 1)
+                
+        def test_expectZeroForEmptyInput(self):
+               self.assertEqual(add("//;\n1;2"), 3)
+                
+        #def test_expectZeroForEmptyInput(self):
+            #    self.assertEqual(add("1\n2,3"),6);
+        
 
+
+
+
+if __name__ == '__main__':
+    unittest.main()
