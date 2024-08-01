cb965853bd3bb717067471ce5f5149f05f64101d
Mohini-1998 <mohinimahalle18@gmail.com>
2024-08-01 12:43:58 +0530

    Update StringCalculatorsecondTest.py

--- StringCalculatorsecondTest.py
+++ StringCalculatorsecondTest.py
@@ -4,19 +4,12 @@ class TestStringCalculator(unittest.TestCase):
                 
         def test_expectZeroForEmptyInput(self):
                 self.assertEqual(add("1,2"), 3)
-                
-        #def test_expectZeroForEmptyInput(self):
-               # self.assertEqual(add("1,1001"), 1)
-                
+     
         def test_expectZeroForEmptyInput(self):
                self.assertEqual(add("//;\n1;2"), 3)
+          
+        def test_expectZeroForEmptyInput(self):
+               self.assertEqual(add("1+2+0"), 3) 
                 
-        #def test_expectZeroForEmptyInput(self):
-            #    self.assertEqual(add("1\n2,3"),6);
-        
-
-
-
-
 if __name__ == '__main__':
     unittest.main()
