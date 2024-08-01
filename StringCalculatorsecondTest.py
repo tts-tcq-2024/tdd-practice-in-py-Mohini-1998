99c219351fdb4b319e666a0418a7fa9391c8e2ef
Mohini-1998 <mohinimahalle18@gmail.com>
2024-08-01 12:46:49 +0530

    Update StringCalculatorsecondTest.py

--- StringCalculatorsecondTest.py
+++ StringCalculatorsecondTest.py
@@ -13,7 +13,7 @@ class TestStringCalculator(unittest.TestCase):
           
         def test_expectZeroForEmptyInput(self):
                self.assertEqual(add("10-2+1"), 3)   
-          "10-2+1"
+        
                 
 if __name__ == '__main__':
     unittest.main()
